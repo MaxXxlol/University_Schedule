221eadb7cccaeefd42649d90692bb907065c8170
maxll <maxllbit@gmail.com>
2016-11-29 15:24:19 +0300

    Add UniversityDB and UniversityModel (expected time: 3hr, elapsed time: 7hr)

---
+++ University_Schedule/SQLQuery2.sql
@@ -0,0 +1,129 @@
+﻿
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 11/29/2016 14:45:31
+-- Generated from EDMX file: C:\Users\Lenovo\Documents\Visual Studio 2015\Projects\Test_web_api\WebApplication1\WebApplication1\Models\UniversittyModel.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [UniversityDB];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'CourseSet'
+CREATE TABLE [dbo].[CourseSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Teacher_Id] int  NOT NULL
+);
+GO
+
+-- Creating table 'StudentSet'
+CREATE TABLE [dbo].[StudentSet] (
+    [Id] int IDENTITY(1,1) NOT NULL
+);
+GO
+
+-- Creating table 'TeacherSet'
+CREATE TABLE [dbo].[TeacherSet] (
+    [Id] int IDENTITY(1,1) NOT NULL
+);
+GO
+
+-- Creating table 'CourseStudent'
+CREATE TABLE [dbo].[CourseStudent] (
+    [Course_Id] int  NOT NULL,
+    [Student_Id] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'CourseSet'
+ALTER TABLE [dbo].[CourseSet]
+ADD CONSTRAINT [PK_CourseSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'StudentSet'
+ALTER TABLE [dbo].[StudentSet]
+ADD CONSTRAINT [PK_StudentSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'TeacherSet'
+ALTER TABLE [dbo].[TeacherSet]
+ADD CONSTRAINT [PK_TeacherSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Course_Id], [Student_Id] in table 'CourseStudent'
+ALTER TABLE [dbo].[CourseStudent]
+ADD CONSTRAINT [PK_CourseStudent]
+    PRIMARY KEY CLUSTERED ([Course_Id], [Student_Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [Teacher_Id] in table 'CourseSet'
+ALTER TABLE [dbo].[CourseSet]
+ADD CONSTRAINT [FK_CourseTeacher]
+    FOREIGN KEY ([Teacher_Id])
+    REFERENCES [dbo].[TeacherSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_CourseTeacher'
+CREATE INDEX [IX_FK_CourseTeacher]
+ON [dbo].[CourseSet]
+    ([Teacher_Id]);
+GO
+
+-- Creating foreign key on [Course_Id] in table 'CourseStudent'
+ALTER TABLE [dbo].[CourseStudent]
+ADD CONSTRAINT [FK_CourseStudent_Course]
+    FOREIGN KEY ([Course_Id])
+    REFERENCES [dbo].[CourseSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating foreign key on [Student_Id] in table 'CourseStudent'
+ALTER TABLE [dbo].[CourseStudent]
+ADD CONSTRAINT [FK_CourseStudent_Student]
+    FOREIGN KEY ([Student_Id])
+    REFERENCES [dbo].[StudentSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_CourseStudent_Student'
+CREATE INDEX [IX_FK_CourseStudent_Student]
+ON [dbo].[CourseStudent]
+    ([Student_Id]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
